8e36fbf0ebf9518b56240affdbba11503e7dff0a
jrgorm <jrgorm@umich.edu>
2015-11-17 20:30:39 -0500

    Create Parameters.m

---
+++ Parameters.m
@@ -0,0 +1,28 @@
+%%% Parameters %%%
+% Updated 11/17/15 JRG
+%
+%% Aircraft geometry
+Sref = 5500; %[ft^2] Wing area
+Lfus = 
+Dfus = 
+%
+%% Fuel Weight parameters
+C = 0.53; %[lb/(lbf*h)] TSFC of GE90-110B1L
+
+%
+%% Drag Polar parameters 
+cf = 
+Swet_Sref = 
+AR = 
+e = 
+%
+%% Design Diagram parameters
+sFL = 
+Ks =
+
+% Climb Gradients
+G_1 = .005;
+G_2 = .024;
+G_3 = .012;
+
+M = 0.83; %Cruise Mach number
