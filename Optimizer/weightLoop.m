9eb1d5321462650f0bbd4c91e589928226b2d536
spanske <spanske@umich.edu>
2015-11-19 21:47:10 -0500

    Update weightLoop.m

--- Optimizer/weightLoop.m
+++ Optimizer/weightLoop.m
@@ -1,20 +1,46 @@
-MTOW = 700000; %%% inital MTOW guess
-T0 = 200000; %%% intial thrust guess
+%%%%% Optimizer: Weight Estimation Inner Loop 
 
-WfoverMTOW = .6; %%%% fuel weight over MTOW (from john's code) : UPDATE TO ACTUAL VALUE
+MTOW = 700000;%%% inital MTOW guess
+wNew = 1.1*MTOW; %%% initialize a dummy weight variable so loop will run
+W0 = wNew;  %%% initialize the new output weight variable
 
-tolerance = .1; %%%% weight error tolerance
+T0 = 200000; %%% INPUT!!!! : total thrust : initial guess, will need to be updated with 2nd outer thrust loop
 
-while (MTOW - MTOWnew) > tolerance
+%%% Need to define these in parameters
+wCrew = 14*180 + 30*14;
+wPayload = 100*125;
+wPassengers = 180*125+125*20;
+
+Total = [ ]; % for troubleshooting table
+Fuel = [ ]; % for troubleshooting table
+Empty = [ ]; % for troubleshooting table
+
+while abs(MTOW - W0) >= .1  
     
-    
+MTOW = wNew;
+%%% function call to EmptyweightIII, need to define input to this function
+%%% in a common parameters file somewhere (see EmptyweightIII for details
+%%% on inputs/outputs to this function
 [WeoverMTOW] = EmptyweightIII(T0,5000,2000,1200,MTOW,9,35,.2,.10,6000,1200,2000,800);
 
-Wfuel = WfoverMTOW * MTOW;
-Wempty = WeoverMTOW * MTOW;
+%%% Compute Fuel and Empty Weights
+WfoverMTOW = .52; %%%% INPUT!!!!: fuel weight over MTOW (from john's code) : will need to be found from John's fuel code
 
-MTOWnew = Wfuel + Wempty;
+Wfuel = WfoverMTOW * MTOW;  %%%% Note, WfoverMTOW will be found from a function call (above this line) of John's fuel weight code
+Wempty = WeoverMTOW * MTOW; %%% WeoverMTOW was found in the function call of EmptyweightIII (above)
 
-MTOW = MTOWnew;
+wNew = Wfuel + Wempty + wCrew + wPayload + wPassengers; %%% Sum all weights to get MTOW
+
+Total = [Total;wNew]; % for troubleshooting table
+Fuel = [Fuel;Wfuel]; % for troubleshooting table
+Empty = [Empty;Wempty]; % for troubleshooting table
+
+W0 = wNew;
 
 end
+% troubleshooting table
+T = table(Total,Fuel,Empty,'VariableNames',{'Total' 'Fuel' 'Empty' })
+
+%%%%% INNER LOOP OUTPUT:
+MTOW = W0; %% outputs final MTOW after loop completes
+
