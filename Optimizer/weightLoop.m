62a18133932d5a5a88dc0ab497e28a285ee9ea5c
Seth Burke <burkeset@umich.edu>
2015-11-19 22:55:54 -0500

    Standardized input/output for integration

--- Optimizer/weightLoop.m
+++ Optimizer/weightLoop.m
@@ -1,19 +1,22 @@
+function [ W0 ] = weightLoop( parameters, thrustParameters,tailParameters)
+%UNTITLED11 Summary of this function goes here
+%   Detailed explanation goes here
 %%%%% Optimizer: Weight Estimation Inner Loop 
 
-MTOW = 700000;%%% inital MTOW guess
+MTOW = parameters.W0;%%% inital MTOW guess
 wNew = 1.1*MTOW; %%% initialize a dummy weight variable so loop will run
 W0 = wNew;  %%% initialize the new output weight variable
 
-T0 = 200000; %%% INPUT!!!! : total thrust : initial guess, will need to be updated with 2nd outer thrust loop
+% T0 = thrustParameters.T0; %%% INPUT!!!! : total thrust : initial guess, will need to be updated with 2nd outer thrust loop
 
 %%% Need to define these in parameters
-wCrew = 14*180 + 30*14;
-wPayload = 100*125;
-wPassengers = 180*125+125*20;
+wCrew = parameters.wCrew;
+wPayload = parameters.wPayload;
+wPassengers = parameters.wPassengers;
 
-Total = [ ]; % for troubleshooting table
-Fuel = [ ]; % for troubleshooting table
-Empty = [ ]; % for troubleshooting table
+% Total = [ ]; % for troubleshooting table
+% Fuel = [ ]; % for troubleshooting table
+% Empty = [ ]; % for troubleshooting table
 
 while abs(MTOW - W0) >= .1  
     
@@ -21,9 +24,12 @@ MTOW = wNew;
 %%% function call to EmptyweightIII, need to define input to this function
 %%% in a common parameters file somewhere (see EmptyweightIII for details
 %%% on inputs/outputs to this function
-[WeoverMTOW] = EmptyweightIII(T0,5000,2000,1200,MTOW,9,35,.2,.10,6000,1200,2000,800);
+[WeoverMTOW] = EmptyweightIII(parameters,tailParameters,thrustParameters);
 
 %%% Compute Fuel and Empty Weights
+
+%RUN FUEL WEIGHT CODExxxxxxxxxxxxxxxxxxxxxxxxxx
+%xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 WfoverMTOW = .52; %%%% INPUT!!!!: fuel weight over MTOW (from john's code) : will need to be found from John's fuel code
 
 Wfuel = WfoverMTOW * MTOW;  %%%% Note, WfoverMTOW will be found from a function call (above this line) of John's fuel weight code
@@ -31,16 +37,16 @@ Wempty = WeoverMTOW * MTOW; %%% WeoverMTOW was found in the function call of Emp
 
 wNew = Wfuel + Wempty + wCrew + wPayload + wPassengers; %%% Sum all weights to get MTOW
 
-Total = [Total;wNew]; % for troubleshooting table
-Fuel = [Fuel;Wfuel]; % for troubleshooting table
-Empty = [Empty;Wempty]; % for troubleshooting table
+% Total = [Total;wNew]; % for troubleshooting table
+% Fuel = [Fuel;Wfuel]; % for troubleshooting table
+% Empty = [Empty;Wempty]; % for troubleshooting table
 
 W0 = wNew;
 
 end
 % troubleshooting table
-T = table(Total,Fuel,Empty,'VariableNames',{'Total' 'Fuel' 'Empty' })
+%T = table(Total,Fuel,Empty,'VariableNames',{'Total' 'Fuel' 'Empty' })
 
-%%%%% INNER LOOP OUTPUT:
-MTOW = W0; %% outputs final MTOW after loop completes
+
+end
 
