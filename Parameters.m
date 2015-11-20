33923bcf4725d57efb27ad58370dce3bb409a18d
jrgorm <jrgorm@umich.edu>
2015-11-19 21:56:22 -0500

    Update Parameters.m

--- Parameters.m
+++ Parameters.m
@@ -1,50 +1,52 @@
 %%% Parameters %%%
-% Updated 11/17/15 JRG
+% Updated 11/19/15 JRG
 %
 %% Mission parameters
-cruise_h = 35000; %[ft] Cruise altitude
+cruise_h = 10668; %[m] Cruise altitude
+hbase = 10.668; %[m] Altitude at start of climb
 M_cruise = 0.83;
-a_cruise= 973.1; %[ft/s] Speed of sound @ 35000 ft (std atm table)
-V_cruise = M_cruise*a_cruise; %[ft/s] Cruise velocity
-
-rho_EWR = 0.0024389826; %[slugs/ft^3] Air Density @ Newark
-rho_SIN = 0.00218596; %[slugs/ft^3] Air Density @ Singapore
-rho_cruise = 0.000736627; %[slugs/ft^3] Air density @ 35,000 ft
-rho_SL = 0.0023768924; %[slugs/ft^3] Air Density @ PDAS Standard Atmosphere Sea Level
+a_cruise= 296.60088; %[m/s] Speed of sound @ 35000 ft (std atm table)
+V_cruise = M_cruise*a_cruise; %[m/s] Cruise velocity
+R = 15343820; %[m] Mission range (8285 nmi)
+rho_EWR = 1.25699997; %[kg/m^3] Air Density @ Newark
+rho_SIN = 1.12659748; %[kg/m^3] Air Density @ Singapore
+rho_cruise = 0.379641953; %[kg/m^3] Air density @ 35,000 ft
+rho_SL = 1.225; %[kg/m^3] Air Density @ PDAS Standard Atmosphere Sea Level
 
 E_loiter = 0.5; %[hr] Duration of loiter phase
-R_alt = 200; %[nmi] Alternate range
-V_alt = 250; %[ft/s] Alternate cruise velocity
+R_alt = 370400; %[m] Alternate range
+V_alt = 75; %[m/s] Alternate cruise velocity
 %
 %% Aircraft geometry
-Sref = 5500; %[ft^2] Wing area
-Lfus = 
-Dfus = 
+Sref = 511.79; %[m^2] Wing area
+% Lfus = 
+% Dfus = 
 %
 %% Fuel Weight parameters
-C = 0.53; %[lb/(lbf*h)] TSFC of GE90-110B1L
-C_loiter = ???
-C_alt = ???
-T0 = 110000; %[lbf] Maximum thrust per engine
-W0 = 729715; %[lb] MTOW
+C = 0.53; %[1/hr] TSFC of GE90-110B1L
+C_loiter = .5
+C_alt = .5
+T0 = 469295; %[N] Maximum thrust per engine
+W0 = 3243732.93149059; %[N] MTOW
+V_stall = 38.89; %[m/s] Stall speed
 %
 %% Drag Polar parameters 
-cf = 
-Swet_Sref = 
-AR = 
-e = 
-CD0_climb =
-K_climb = 
-CD0_cruise = 
-K_cruise = 
-CD0_loiter =
-K_loiter = 
-L_D_max =
-L_D_alt = 
+% cf = 
+% Swet_Sref = 
+% AR = 
+e = .9;
+CD0_climb =.03;
+K_climb = 0.044209706414415;
+CD0_cruise = .0177;
+K_cruise = 0.041609135448862;
+CD0_loiter =.0177;
+K_loiter = 0.041609135448862;
+L_D_max = 16;
+L_D_alt = 10;
 %
 %% Design Diagram parameters
-sFL = 
-Ks =
+% sFL = 
+% Ks =
 
 % Climb Gradients
 G_1 = .005;
