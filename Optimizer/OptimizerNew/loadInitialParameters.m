3a401b2c1379d8c5843fb622e02f6d24fc431569
Seth Burke <burkeset@umich.edu>
2015-11-24 17:59:18 -0500

    Create loadInitialParameters.m

---
+++ Optimizer/OptimizerNew/loadInitialParameters.m
@@ -0,0 +1,7 @@
+function [tailParameters] = loadInitialParameters()
+%Initial defined tail parameters
+
+    tailParameters.Cvt = 0.0577;   %%%% ADJUST THIS IF NEEDED: Vertical Tail Volume Coefficient: BASED ON 777-100X1
+    tailParameters.Cht = 0.8385; %%%% ADJUST THIS IF NEEDED: Horizontal Tail Volume Coefficient: : BASED ON 777-100X1
+
+end
